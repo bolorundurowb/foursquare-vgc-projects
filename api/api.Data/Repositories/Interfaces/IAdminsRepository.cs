edf2660da6bbff34bd1229b43a80f79f204365b5
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-08 14:52:07 +0100

    ft: update interface

--- api/api.Data/Repositories/Interfaces/IAdminsRepository.cs
+++ api/api.Data/Repositories/Interfaces/IAdminsRepository.cs
@@ -1,8 +1,10 @@
-﻿using api.Data.Models;
+﻿using System.Threading.Tasks;
+using api.Data.Models;
 
 namespace api.Data.Repositories.Interfaces
 {
     public interface IAdminsRepository : IRepository<Admin>
     {
+        Task<Admin> Login(string email);
     }
 }
