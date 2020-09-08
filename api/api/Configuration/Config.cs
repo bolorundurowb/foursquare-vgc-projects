b688f2c5d173112c15136b8c20967aa7c47e7ece
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-08 23:30:15 +0100

    ft: add in config

---
+++ api/api/Configuration/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace api.Configuration
+{
+    public static class Config
+    {
+        public static string DbServerUrl => Environment.GetEnvironmentVariable("DB_SERVER");
+
+        public static string DbName => Environment.GetEnvironmentVariable("DB_NAME");
+
+        public static string Secret => Environment.GetEnvironmentVariable("SECRET");
+
+        public static string Audience => "foursquare-neophyte";
+
+        public static string Issuer => "foursquare-neophyte";
+    }
+}
