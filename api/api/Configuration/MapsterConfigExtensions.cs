348d353dbc8c3c56491dcfca49fb740d9ca7a2b8
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-08 23:39:43 +0100

    ft: more startup scaffolding

---
+++ api/api/Configuration/MapsterConfigExtensions.cs
@@ -0,0 +1,16 @@
+﻿using api.Data.Models;
+using api.Models.View;
+using Mapster;
+
+namespace api.Configuration
+{
+    public static class MapsterConfigExtensions
+    {
+        public static void ConfigureMappings(TypeAdapterConfig config)
+        {
+            config
+                .NewConfig<Admin, AdminViewModel>()
+                .Map(x => x.Id, y => y.Id.ToString());
+        }
+    }
+}
--- api/api/Startup.cs
+++ api/api/Startup.cs
@@ -1,31 +1,85 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System.Text.Json.Serialization;
+using api.Configuration;
+using api.Data.Repositories.Implementations;
+using api.Data.Repositories.Interfaces;
+using dotenv.net.DependencyInjection.Microsoft;
+using logly.Extensions;
+using Mapster;
+using MapsterMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        private readonly IWebHostEnvironment _environment;
 
-        public IConfiguration Configuration { get; }
+        public Startup(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            // read the environment vars
+            services.AddEnv(builder =>
+            {
+                builder
+                    .AddEncoding(Encoding.Default)
+                    .AddEnvFile(Path.GetFullPath(".env"))
+                    .AddThrowOnError(_environment.IsDevelopment());
+            });
+            services.AddEnvReader();
+
+            services.AddCors();
+
+            services.AddRouting(option => option.LowercaseUrls = true);
+
+            services.AddControllers()
+                .AddJsonOptions(x => x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Neopyhte API",
+                    Version = "v1"
+                });
+            });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opts =>
+                {
+                    opts.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = true,
+                        ValidateIssuer = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidAudience = Config.Audience,
+                        ValidIssuer = Config.Issuer,
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Config.Secret))
+                    };
+                });
+
+            // add in mapper
+            var config = TypeAdapterConfig.GlobalSettings;
+            services.AddSingleton(config);
+            services.AddScoped<IMapper, ServiceMapper>();
+            MapsterConfigExtensions.ConfigureMappings(config);
+
+            // add DI mappings
+            services.AddScoped<IAdminsRepository, AdminsRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,9 +90,31 @@ namespace api
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseLogly(opts => opts
+                .AddRequestMethod()
+                .AddStatusCode()
+                .AddResponseTime()
+                .AddUrl()
+                .AddResponseLength());
+
+            app.UseCors(options => options
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin());
+
             app.UseHttpsRedirection();
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Neophyte API");
+                c.RoutePrefix = "docs";
+            });
+
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
