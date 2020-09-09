81ea8c28ca8415417ad36c7d7c518a725da94a6a
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-09 19:07:47 +0100

    ft: add endpoint for registring attendance

---
+++ api/api/Controllers/AttendanceController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using api.Data.Repositories.Interfaces;
+using api.Models.Binding;
+using api.Models.View;
+using MapsterMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Authorize]
+    [Microsoft.AspNetCore.Components.Route("v1/attendance")]
+    public class AttendanceController : BaseController
+    {
+        private readonly IAttendanceRepository _attendanceRepo;
+
+        public AttendanceController(IMapper mapper, IAttendanceRepository attendanceRepo) : base(mapper)
+        {
+            _attendanceRepo = attendanceRepo;
+        }
+
+        [HttpPost("")]
+        [ProducesResponseType(typeof(AttendeeViewModel), 201)]
+        public async Task<IActionResult> AddAttendee([FromBody] AttendeeRegistrationBindingModel bm)
+        {
+            var attendee = await _attendanceRepo.AddAttendee(bm.FullName, bm.HomeAddress, bm.Phone, bm.EmailAddress,
+                bm.BirthDay, bm.Gender, bm.AgeGroup, bm.CommentsOrPrayers, bm.HowYouFoundUs, bm.BornAgain,
+                bm.BecomeMember, bm.Remarks);
+            return Created(Mapper.Map<AttendeeViewModel>(attendee));
+        }
+    }
+}
