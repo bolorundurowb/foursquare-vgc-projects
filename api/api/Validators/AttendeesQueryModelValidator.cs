d1dc2fa7ff46a27d8a15b948cb3884ff71d8d419
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-09 20:06:36 +0100

    ft: add in query model and validator

---
+++ api/api/Validators/AttendeesQueryModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using api.Models.Binding;
+using FluentValidation;
+
+namespace api.Validators
+{
+    public class AttendeesQueryModelValidator : AbstractValidator<AttendeesQueryModel>
+    {
+        public AttendeesQueryModelValidator()
+        {
+            RuleFor(x => x.Date)
+                .NotEqual(DateTime.MinValue)
+                .WithMessage("Invalid date")
+                .NotEqual(DateTime.MaxValue)
+                .WithMessage("Invalid date");
+        }
+    }
+}
