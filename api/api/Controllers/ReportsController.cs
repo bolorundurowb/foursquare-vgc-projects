0bde462bcbab3074ecf34924708fe197b2a5cc9f
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-13 18:40:48 +0100

    ft: finalize report generation

--- api/api/Controllers/ReportsController.cs
+++ api/api/Controllers/ReportsController.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using api.Configuration;
 using api.Data.Helpers;
 using api.Data.Repositories.Interfaces;
+using api.Shared.Email.Interfaces;
+using api.Shared.Email.Models;
 using MapsterMapper;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,22 +15,48 @@ namespace api.Controllers
     {
         private readonly IAttendanceRepository _attendanceRepo;
         private readonly INewcomersRepository _newcomersRepo;
-        
-        public ReportsController(IMapper mapper, IAttendanceRepository attendanceRepo, INewcomersRepository newcomersRepo) : base(mapper)
+        private readonly IEmailService _emailService;
+
+        public ReportsController(IMapper mapper, IAttendanceRepository attendanceRepo,
+            INewcomersRepository newcomersRepo, IEmailService emailService) : base(mapper)
         {
             _attendanceRepo = attendanceRepo;
             _newcomersRepo = newcomersRepo;
+            _emailService = emailService;
         }
 
         [HttpPost("{date:DateTime")]
         [ProducesResponseType(200)]
         public async Task<IActionResult> GenerateReports(DateTime date)
         {
+            var formattedDateString = date.Date.ToString("yyyy-MM-dd");
             var attendance = await _attendanceRepo.GetAttendance(date);
             var newcomers = await _newcomersRepo.GetNewcomers(date);
 
             var attendanceCsv = await CsvHelpers.GenerateCsvFromAttendance(attendance);
             var newcomerCsv = await CsvHelpers.GenerateCsvFromNewcomers(newcomers);
+
+            var emailMessage = new EmailMessage
+            {
+                Subject = $"Reports for {formattedDateString}",
+                Content = "<p>See attached for reports</p>",
+                Attachments = new List<EmailAttachment>
+                {
+                    new EmailAttachment
+                    {
+                        Content = attendanceCsv,
+                        MimeType = "text/csv",
+                        Name = $"{formattedDateString}-attendance.csv"
+                    },
+                    new EmailAttachment
+                    {
+                        Content = newcomerCsv,
+                        MimeType = "text/csv",
+                        Name = $"{formattedDateString}-newcomers.csv"
+                    }
+                }
+            };
+            await _emailService.SendAsync(Config.DestinationEmail, emailMessage);
 
             return Ok();
         }
