4bee090a135786c38e54a7c8384bb831e4595d3e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-13 17:22:01 +0100

    ft: add in email service

---
+++ api/api.Shared/Email/Interfaces/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using api.Shared.Email.Models;
+
+namespace api.Shared.Email.Interfaces
+{
+    public interface IEmailService
+    {
+        Task<bool> SendAsync(string recipient, EmailMessage emailMessage);
+    }
+}
