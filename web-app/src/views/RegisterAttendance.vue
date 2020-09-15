8966bf3e62717669304df31b9ffe4baa0b9ac0ad
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-15 10:24:06 +0100

    ft: add in other fields

--- web-app/src/views/RegisterAttendance.vue
+++ web-app/src/views/RegisterAttendance.vue
@@ -3,27 +3,69 @@
     <div class="content">
       <div class="logo">
         <img src="../assets/logo.png"/>
-        <h2>Register</h2>
-        <h6>Reserve a Seat</h6>
+        <h3>Register</h3>
+        <h6>Reserve a seat for next sunday</h6>
       </div>
 
       <div class="form">
         <form>
           <fieldset>
-            <label for="nameField">Name</label>
-            <input type="text" placeholder="CJ Patoilo" id="nameField"/>
-            <label for="ageRangeField">Age Range</label>
-            <select id="ageRangeField">
-              <option value="0-13">0-13</option>
-              <option value="14-17">14-17</option>
-              <option value="18-23">18-23</option>
-              <option value="24+">24+</option>
-            </select>
+            <div class="question-line">
+              <input type="checkbox">
+              <label class="label-inline">Did you return from a trip overseas in the last 10 days?</label>
+            </div>
 
-            <label for="commentField">Comment</label>
-            <textarea placeholder="Hi CJ …" id="commentField"></textarea>
+            <div class="question-line">
+              <input type="checkbox">
+              <label class="label-inline">Do you live with COVID-19 caregivers?</label>
+            </div>
 
-            <input class="button-primary" type="submit" value="Submit"/>
+            <div class="question-line">
+              <input type="checkbox">
+              <label class="label-inline">Do you recently care for any sick individual at home or in a hospital?</label>
+            </div>
+
+            <div>Do you presently have a cold, fever, sore throat, loss of smell or loss of appetite?</div>
+            <div class="question-line">
+              <input type="radio" name="has-symptoms">
+              <label class="label-inline">Yes</label>
+              <input class="radio" type="radio" name="has-symptoms">
+              <label class="label-inline">No</label>
+              <input class="radio" type="radio" name="has-symptoms">
+              <label class="label-inline">Maybe</label>
+            </div>
+
+            <label>Full Name:</label>
+            <input type="text"/>
+
+            <label>Email Address:</label>
+            <input type="email"/>
+
+            <div class="question-line row">
+              <div class="column">
+                <label>Age (in years):</label>
+                <input type="number"/>
+              </div>
+              <div class="column">
+                <label>Gender:</label>
+                <select>
+                  <option value="Other">Other</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+              </div>
+            </div>
+
+            <label>Phone Number:</label>
+            <input type="tel"/>
+
+            <label>Residential Address:</label>
+            <textarea
+                rows="3"></textarea>
+
+            <input class="button-primary"
+                   type="submit"
+                   value="Submit"/>
           </fieldset>
         </form>
       </div>
@@ -52,7 +94,11 @@ export default {
       text-align: center;
 
       img {
-        width: 15rem;
+        width: 10rem;
+      }
+
+      h6 {
+        margin-bottom: 1.2rem;
       }
     }
   }
@@ -61,6 +107,15 @@ export default {
     background-color: #fff;
     border-radius: 0.5rem;
     padding: 3rem 3rem 1.5rem;
+
+    .question-line {
+      margin-top: 0.5rem;
+      margin-bottom: 0.5rem;
+    }
+
+    .radio {
+      margin-left: 1rem;
+    }
   }
 }
 
