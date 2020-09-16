bfc791d0375d62e738ab659af0ce0b355fdafa68
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-16 11:24:01 +0100

    ft: ad din validator for the attendee reg

---
+++ api/api/Validators/AttendeeRegistrationBindingModelValidator.cs
@@ -0,0 +1,15 @@
+﻿using api.Models.Binding;
+using FluentValidation;
+
+namespace api.Validators
+{
+    public class AttendeeRegistrationBindingModelValidator : AbstractValidator<AttendeeRegistrationBindingModel>
+    {
+        public AttendeeRegistrationBindingModelValidator()
+        {
+            RuleFor(x => x.FullName)
+                .NotEmpty()
+                .WithMessage("A full name is required.");
+        }
+    }
+}
