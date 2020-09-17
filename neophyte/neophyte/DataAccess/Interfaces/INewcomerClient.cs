346adea1e434e9d29c32da09a0b9f8c959bc0131
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-17 14:18:30 +0100

    ft: add in view and binding models

---
+++ neophyte/neophyte/DataAccess/Interfaces/INewcomerClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using neophyte.Models.Binding;
+using neophyte.Models.View;
+using Refit;
+
+namespace neophyte.DataAccess.Interfaces
+{
+    public interface INewcomerClient
+    {
+        [Get("/newcomers")]
+        [Headers("Authorization: Bearer")]
+        Task<DateSummaryViewModel[]> Get();
+        
+        [Post("/newcomers")]
+        Task Register([Body] NewcomerBindingModel payload);
+        
+        [Get("/newcomers/{date}")]
+        [Headers("Authorization: Bearer")]
+        Task<NewcomerViewModel[]> GetNewcomersForDate(string date);
+        
+        [Delete("/newcomers/{id}")]
+        [Headers("Authorization: Bearer")]
+        Task DeleteAttendee(string id);
+    }
+}
