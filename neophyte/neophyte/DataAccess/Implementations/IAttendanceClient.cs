78f60691bef16d5f43bfffc827ce78ea0fcf1a03
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-17 11:31:08 +0100

    ft: add in iattendance client interfavce

---
+++ neophyte/neophyte/DataAccess/Implementations/IAttendanceClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using neophyte.Models.View;
+using Refit;
+
+namespace neophyte.DataAccess.Implementations
+{
+    public interface IAttendanceClient
+    {
+        [Get("/attendance")]
+        [Headers("Authorization: Bearer")]
+        Task<DateSummaryViewModel[]> Get();
+    }
+}
