7f58ed4045cab26151cba0f18bc210ad41047818
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-17 08:52:08 +0100

    ft: add in login bm and interface for auth

---
+++ neophyte/neophyte/DataAccess/Interfaces/IAuthClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using neophyte.Models.Binding;
+using Refit;
+
+namespace neophyte.DataAccess.Interfaces
+{
+    public interface IAuthClient
+    {
+        [Post("/auth/login")]
+        Task<> Login(LoginBindingModel payload);
+    }
+}
