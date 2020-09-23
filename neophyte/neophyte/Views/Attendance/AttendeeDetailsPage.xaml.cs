d19fb72cae7603a4e1078a97dbfc39baf2f0c1ad
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-23 11:29:58 +0100

    ft: add in fields necessary for edits

--- neophyte/neophyte/Views/Attendance/AttendeeDetailsPage.xaml.cs
+++ neophyte/neophyte/Views/Attendance/AttendeeDetailsPage.xaml.cs
@@ -1,4 +1,7 @@
+using MapsterMapper;
+using neophyte.DataAccess.Implementations;
 using neophyte.Models.View;
+using neophyte.Validators;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,12 +10,27 @@ namespace neophyte.Views.Attendance
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AttendeeDetailsPage : ContentPage
     {
+        private readonly AttendanceClient _attendanceClient;
+        private readonly AttendanceValidator _attendanceValidator = new AttendanceValidator();
+        private readonly IMapper _mapper = new Mapper();
+
         public AttendeeDetailsPage(AttendeeViewModel attendee)
         {
             InitializeComponent();
 
             Title = "Attendee Details";
             SetValue(NavigationPage.BarBackgroundColorProperty, Color.FromHex("#52004C"));
+
+            // set dropdown values
+            // cmbGender.ItemsSource = Constants.Genders;
+
+            // initialize stuff
+            _attendanceClient = new AttendanceClient();
+            SetAttendeeDisplayValue(attendee);
+        }
+
+        private void SetAttendeeDisplayValue(AttendeeViewModel attendee)
+        {
             BindingContext = attendee;
         }
     }
