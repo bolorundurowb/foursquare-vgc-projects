5c330b49252a9c95812dbb55b30e3baf648d130f
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-03-08 12:41:16 +0100

    ft: fix option display

--- neophyte/neophyte.Android/Resources/Resource.designer.cs
+++ neophyte/neophyte.Android/Resources/Resource.designer.cs
@@ -11438,13 +11438,16 @@ namespace neophyte.Droid
 			public const int register = 2131165314;
 			
 			// aapt resource value: 0x7F070083
-			public const int team = 2131165315;
+			public const int sendreport = 2131165315;
 			
 			// aapt resource value: 0x7F070084
-			public const int tooltip_frame_dark = 2131165316;
+			public const int team = 2131165316;
 			
 			// aapt resource value: 0x7F070085
-			public const int tooltip_frame_light = 2131165317;
+			public const int tooltip_frame_dark = 2131165317;
+			
+			// aapt resource value: 0x7F070086
+			public const int tooltip_frame_light = 2131165318;
 			
 			static Drawable()
 			{
