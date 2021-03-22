358963c4ca0ccbdaead2d71581e50e0472938386
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-03-22 16:03:49 +0100

    ft: add support for the attendees

--- api/api/Utils/ExcelGenerator.cs
+++ api/api/Utils/ExcelGenerator.cs
@@ -13,6 +13,7 @@ namespace api.Utils
             using (var workbook = new XLWorkbook())
             {
                 var worksheet = workbook.Worksheets.Add("Sheet 1");
+                
                 // add header
                 AddHeader(worksheet);
 
@@ -32,7 +33,7 @@ namespace api.Utils
                     worksheet.Cell(row + 1, 11).Value = newcomer.HowYouFoundUs;
                     worksheet.Cell(row + 1, 12).Value = newcomer.Remarks;
                     
-                    // increement row counter
+                    // increment row counter
                     row++;
                 }
 
@@ -54,6 +55,54 @@ namespace api.Utils
                 worksheet.Cell(1,10).Value = "Comments/Prayers";
                 worksheet.Cell(1,11).Value = "How did you find out about us?";
                 worksheet.Cell(1,12).Value = "Remarks";
+            }
+        }
+        
+        public static byte[] FromAttendees(IEnumerable<Attendee> attendees)
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Sheet 1");
+                
+                // add header
+                AddHeader(worksheet);
+
+                var row = 1;
+                foreach (var attendee in attendees)
+                {
+                    worksheet.Cell(row + 1, 1).Value = row;
+                    worksheet.Cell(row + 1, 2).Value = attendee.FullName;
+                    worksheet.Cell(row + 1, 3).Value = attendee.EmailAddress;
+                    worksheet.Cell(row +1, 4).Value = attendee.Phone;
+                    worksheet.Cell(row + 1, 5).Value = attendee.ResidentialAddress;
+                    worksheet.Cell(row + 1, 6).Value = attendee.SeatNumber.HasValue ? attendee.SeatNumber.ToString() : "N/A";
+                    worksheet.Cell(row + 1, 7).Value = attendee.Gender.GetValueOrDefault().ToString();
+                    worksheet.Cell(row + 1, 8).Value = attendee.ReturnedInLastTenDays ? "Yes" : "No";
+                    worksheet.Cell(row + 1, 9).Value = attendee.LiveWithCovidCaregivers ? "Yes" : "No";
+                    worksheet.Cell(row + 1, 10).Value = attendee.CaredForSickPerson ? "Yes" : "No";
+                    worksheet.Cell(row + 1, 11).Value = attendee.HaveCovidSymptoms.GetValueOrDefault().ToString();
+                    
+                    // increment row counter
+                    row++;
+                }
+
+                return ConvertWorkSheetToByteArray(workbook);
+            }
+
+            static void AddHeader(IXLWorksheet worksheet)
+            {
+                worksheet.Range(1, 1, 1, 13).Style.Font.Bold = true;
+                worksheet.Cell(1,1).Value = "S/No";
+                worksheet.Cell(1,2).Value = "Full Name";
+                worksheet.Cell(1,3).Value = "Email Address";
+                worksheet.Cell(1,4).Value = "Phone Number";
+                worksheet.Cell(1,5).Value = "Residential Address";
+                worksheet.Cell(1,6).Value = "Seat Number";
+                worksheet.Cell(1,7).Value = "Gender";
+                worksheet.Cell(1,8).Value = "Did you return from an overseas trip in the last 10 days?";
+                worksheet.Cell(1,9).Value = "Do you live with COVID-19 caregivers?";
+                worksheet.Cell(1,10).Value = "Have you cared for any sick person recently?";
+                worksheet.Cell(1,11).Value = "Do you have a cough, catarrh or sore-throat?";
             }
         }
 
