cda22255521245ee8455ec288a03c2d5a6fc4b42
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-04-15 17:30:33 +0100

    ft: add in the view model

---
+++ neophyte/neophyte/Models/View/SettingsViewModel.cs
@@ -0,0 +1,11 @@
+namespace neophyte.Models.View
+{
+    public class SettingsViewModel
+    {
+        public string LastLogin { get; set; }
+        
+        public string LoginExpiresAt { get; set; }
+        
+        public string EmailAddress { get; set; }
+    }
+}
