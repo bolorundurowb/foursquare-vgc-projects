2f2ac7ea540292cabd060d46afbdc7e28202430d
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-06-01 13:09:51 +0100

    ch:clean up

--- api/api/Configuration/Extensions/DocumentationExtensions.cs
+++ api/api/Configuration/Extensions/DocumentationExtensions.cs
@@ -1,6 +1,4 @@
-﻿using api.Configuration.Extensions.SwaggerFilters;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 
