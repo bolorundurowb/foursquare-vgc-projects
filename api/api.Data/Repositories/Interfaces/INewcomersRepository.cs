954c7cbf4abe1d9e8d1c311c908cbe08968c0873
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-06-01 09:51:02 +0100

    ft: update the newcomer creation repo method to reflect optinal values

--- api/api.Data/Repositories/Interfaces/INewcomersRepository.cs
+++ api/api.Data/Repositories/Interfaces/INewcomersRepository.cs
@@ -10,17 +10,18 @@ namespace api.Data.Repositories.Interfaces
     public interface INewcomersRepository : IRepository<Newcomer>
     {
         Task<List<DateSummaryDto>> GetNewcomersDates();
-        
+
         Task<List<Newcomer>> GetNewcomers(DateTime date);
-        
+
         Task<Newcomer> AddNewcomer(string fullName, string homeAddress, string phone, string email, string birthDay,
+            string ageGroup, string commentsOrPrayers, string howYouFoundUs, MultiChoice? becomeMember,
+            Gender? gender = null, MultiChoice? bornAgain = null);
+
+        Task<Newcomer> UpdateNewcomer(string id, DateTime? date, string fullName, string homeAddress, string phone,
+            string email, string birthDay,
             Gender? gender, string ageGroup, string commentsOrPrayers, string howYouFoundUs, MultiChoice? bornAgain,
             MultiChoice? becomeMember, string remarks);
-        
-        Task<Newcomer> UpdateNewcomer(string id, DateTime? date, string fullName, string homeAddress, string phone, string email, string birthDay,
-            Gender? gender, string ageGroup, string commentsOrPrayers, string howYouFoundUs, MultiChoice? bornAgain,
-            MultiChoice? becomeMember, string remarks);
-        
+
         Task RemoveNewcomer(string id);
     }
 }
