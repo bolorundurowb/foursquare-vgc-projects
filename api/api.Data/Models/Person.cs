3d2b35a4cf2edbb30692081acf6567c18ee5e4e3
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-06-01 17:42:07 +0100

    ft: add person model

---
+++ api/api.Data/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace api.Data.Models
+{
+    public class Person : IEntity
+    {
+        [BsonId] 
+        public ObjectId Id { get; private set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Phone { get; set; }
+
+        private Person()
+        {
+        }
+
+        public Person(string firstName, string lastName, string phone)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Phone = phone;
+        }
+    }
+}
