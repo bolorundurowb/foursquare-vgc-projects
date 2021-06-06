5a48239149b418470415b53a7ca10a9f1afb9471
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-06-06 16:04:18 +0100

    ft: add the rollbar logging extensions

---
+++ api/api/Configuration/Extensions/LoggingExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Rollbar.NetCore.AspNet;
+
+namespace api.Configuration.Extensions
+{
+    internal static class LoggingExtensions
+    {
+        public static void ConfigureLogging(this IServiceCollection services)
+        {
+            services.AddRollbarLogger(loggerOptions =>
+            {
+                loggerOptions.Filter =
+                    (loggerName, loglevel) => loglevel >= LogLevel.Trace;
+            });
+        }
+
+        public static void UseLogging(this IApplicationBuilder app)
+        {
+            app.UseRollbarMiddleware();
+        }
+    }
+}
