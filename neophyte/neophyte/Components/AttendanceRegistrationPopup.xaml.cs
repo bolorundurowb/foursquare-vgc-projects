f362f1384616cbbeaf73500c41944ea26ac69534
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-06-10 11:28:32 +0100

    ft: add popup details

--- neophyte/neophyte/Components/AttendanceRegistrationPopup.xaml.cs
+++ neophyte/neophyte/Components/AttendanceRegistrationPopup.xaml.cs
@@ -3,18 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Rg.Plugins.Popup.Pages;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace neophyte.Components
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AttendanceRegistrationPopup : ContentPage
+    public partial class AttendanceRegistrationPopup : PopupPage
     {
-        public AttendanceRegistrationPopup()
+        private readonly string _personId;
+        private readonly Action _action;
+        
+        public AttendanceRegistrationPopup(string personId, Action action)
         {
             InitializeComponent();
+
+            _personId = personId;
+            _action = action;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            txtPersonId.Text = _personId;
+        }
+
+        protected async void RegisterAttendance(object sender, EventArgs e)
+        {
+            btnSubmit.IsVisible = false;
+            prgSubmit.IsVisible = true;
+            
         }
     }
 }
