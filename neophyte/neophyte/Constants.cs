e9adbd472fd88172e95fcd21ca6e1ef156d4f7ac
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-06-10 13:51:35 +0100

    ft: switch to v2

--- neophyte/neophyte/Constants.cs
+++ neophyte/neophyte/Constants.cs
@@ -4,10 +4,10 @@ namespace neophyte
 {
     public static class Constants
     {
-        public const string V1BaseUrl = "https://neophyte-dev-api.herokuapp.com/v1";
-        public const string V2BaseUrl = "https://neophyte-dev-api.herokuapp.com/v2";
-        // public const string V1BaseUrl = "https://neophyte-prod-api.herokuapp.com/v1";
-        // public const string V2BaseUrl = "https://neophyte-prod-api.herokuapp.com/v2";
+        // public const string V1BaseUrl = "https://neophyte-dev-api.herokuapp.com/v1";
+        // public const string V2BaseUrl = "https://neophyte-dev-api.herokuapp.com/v2";
+        public const string V1BaseUrl = "https://neophyte-prod-api.herokuapp.com/v1";
+        public const string V2BaseUrl = "https://neophyte-prod-api.herokuapp.com/v2";
 
         public static List<string> Months = new List<string>
         {
