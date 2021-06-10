932144dfd80e1655eb01c05a2fb5cc95e2d7e54a
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-06-10 10:52:53 +0100

    ft: add popup scaffold

---
+++ neophyte/neophyte/Components/AttendanceRegistrationPopup.xaml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace neophyte.Components
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AttendanceRegistrationPopup : ContentPage
+    {
+        public AttendanceRegistrationPopup()
+        {
+            InitializeComponent();
+        }
+    }
+}
