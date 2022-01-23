256884db303bbe62dfa6799642c9d3c6484eb3bc
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-01-23 22:10:07 +0100

    ft: add in venue interface

---
+++ api/api.Data/Repositories/Interfaces/IVenueRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using api.Data.Entities;
+
+namespace api.Data.Repositories.Interfaces;
+
+public interface IVenueRepository
+{
+    Task<Venue> Create(string name, List<string> seatRanges);
+}
