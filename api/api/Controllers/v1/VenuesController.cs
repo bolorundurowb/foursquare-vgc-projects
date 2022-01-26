34599939349d60f592052663e51334516075841b
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-01-26 17:25:34 +0100

    ft: add scaffold

---
+++ api/api/Controllers/v1/VenuesController.cs
@@ -0,0 +1,16 @@
+﻿using api.Data.Repositories.Interfaces;
+using MapsterMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers.v1;
+
+[ApiVersion("1.0")]
+public class VenuesController : ApiController
+{
+    private readonly IVenueRepository _venueRepo;
+
+    public VenuesController(IMapper mapper, IVenueRepository venueRepo) : base(mapper)
+    {
+        _venueRepo = venueRepo;
+    }
+}
