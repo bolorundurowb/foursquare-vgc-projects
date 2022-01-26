dfc1263a567df0c5ec1016feb509e259b27d7e92
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-01-26 17:34:47 +0100

    ft: add in the binding model for creating venues

---
+++ api/api/Models/Binding/SeatRangeBindingModel.cs
@@ -0,0 +1,10 @@
+﻿using api.Data.Enums;
+
+namespace api.Models.Binding;
+
+public class SeatRangeBindingModel
+{
+    public SeatCategory Category { get; set; }
+    
+    public string Number { get; set; }
+}
