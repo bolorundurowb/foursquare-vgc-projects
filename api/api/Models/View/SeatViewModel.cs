fb7ac90d9a6b7af80a3e7c35e03a88a7de37bf72
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-01-26 17:26:21 +0100

    ft: add vew model

---
+++ api/api/Models/View/SeatViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace api.Models.View;
+
+public class SeatViewModel
+{
+    public string Category { get; set; }
+
+    public string Number { get; set; }
+
+    public string AssociatedNumber { get; set; }
+}
