92edb1fe16c1fab7d0094990af4a2beb007d0cb4
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-01-29 23:16:19 +0100

    ft: add person repo support for getting all

--- api/api.Data/Repositories/Implementations/PersonsRepository.cs
+++ api/api.Data/Repositories/Implementations/PersonsRepository.cs
@@ -1,13 +1,28 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using api.Data.Entities;
 using api.Data.Repositories.Interfaces;
 using api.Shared.Extensions;
 using meerkat;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 
 namespace api.Data.Repositories.Implementations;
 
 public class PersonsRepository : IPersonsRepository
 {
+    public Task<List<Person>> GetAll(string name = null)
+    {
+        var queryable = Meerkat.Query<Person>();
+
+        if (!string.IsNullOrWhiteSpace(name))
+            queryable = (IMongoQueryable<Person>)Queryable.Where(queryable,
+                x => x.FirstName.Contains(name) || x.LastName.Contains(name));
+
+        return queryable.ToListAsync();
+    }
+
     public Task<Person> FindById(string personId) => Meerkat.FindByIdAsync<Person>(personId);
 
     public Task<Person> GetByPhone(string phoneNumber)
