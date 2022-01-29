a9559099ba9f3e1dfb29a10db5f75d2d61c37986
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2022-01-29 20:22:19 +0100

    ft: change the venue model and the mapping

--- api/api/Models/View/VenueViewModel.cs
+++ api/api/Models/View/VenueViewModel.cs
@@ -2,11 +2,7 @@
 
 namespace api.Models.View;
 
-public class VenueViewModel
+public class VenueViewModel : BaseVenueViewModel
 {
-    public string Id { get; set; }
-
-    public string Name { get; set; }
-
     public List<SeatViewModel> Seats { get; set; }
 }
