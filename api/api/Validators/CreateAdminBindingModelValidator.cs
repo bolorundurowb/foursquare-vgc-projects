419c37e6c42b292d4cc863ce2e323802e018ca44
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-01-31 13:02:02 +0100

    ft: add validator

---
+++ api/api/Validators/CreateAdminBindingModelValidator.cs
@@ -0,0 +1,20 @@
+﻿using api.Models.Binding;
+using FluentValidation;
+
+namespace api.Validators;
+
+public class CreateAdminBindingModelValidator : AbstractValidator<CreateAdminBindingModel>
+{
+    public CreateAdminBindingModelValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty();
+        
+        RuleFor(x => x.EmailAddress)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty();
+    }
+}
