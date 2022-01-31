34b1fffd50a060f3d5562da84c0a03751f0d7d95
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-01-31 12:58:50 +0100

    ft: add admin repo interface method

--- api/api.Data/Repositories/Implementations/AdminsRepository.cs
+++ api/api.Data/Repositories/Implementations/AdminsRepository.cs
@@ -1,12 +1,19 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using api.Data.Entities;
 using api.Data.Repositories.Interfaces;
 using meerkat;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 
 namespace api.Data.Repositories.Implementations;
 
 public class AdminsRepository : IAdminsRepository
 {
+    public Task<List<Admin>> GetAll() => Meerkat.Query<Admin>()
+        .OrderBy(x => x.Name)
+        .ToListAsync();
+
     public Task<Admin> FindById(string adminId) => Meerkat.FindByIdAsync<Admin>(adminId);
 
     public Task<Admin> FindByEmail(string email)
--- api/api.Data/Repositories/Interfaces/IAdminsRepository.cs
+++ api/api.Data/Repositories/Interfaces/IAdminsRepository.cs
@@ -1,10 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using api.Data.Entities;
 
 namespace api.Data.Repositories.Interfaces;
 
 public interface IAdminsRepository
 {
+    Task<List<Admin>> GetAll();
+
     Task<Admin> FindById(string adminId);
 
     Task<Admin> FindByEmail(string email);
