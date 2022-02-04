89ff95c5ef83df66119f94f4b632129293fe6c96
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-02-04 09:35:52 +0100

    ft: document the admin endpoints

--- api/neophyte-api/Models/Binding/CreateAdminBindingModel.cs
+++ api/neophyte-api/Models/Binding/CreateAdminBindingModel.cs
@@ -4,12 +4,24 @@ namespace neophyte.api.Models.Binding;
 
 public class CreateAdminBindingModel
 {
-    [SwaggerSchema(Nullable = false)] 
+    /// <summary>
+    /// The full name of the admin
+    /// </summary>
+    /// <example>John Doe</example>
+    [SwaggerSchema(Nullable = false)]
     public string Name { get; set; }
 
-    [SwaggerSchema(Nullable = false)] 
+    /// <summary>
+    /// The admins email address
+    /// </summary>
+    /// <example>john@doe.com</example>
+    [SwaggerSchema(Nullable = false)]
     public string EmailAddress { get; set; }
 
-    [SwaggerSchema(Nullable = false)] 
+    /// <summary>
+    /// The default admin of the password
+    /// </summary>
+    /// <example>Y0urP@$5word</example>
+    [SwaggerSchema(Nullable = false)]
     public string Password { get; set; }
 }
--- api/neophyte-api/Models/View/GenericViewModel.cs
+++ api/neophyte-api/Models/View/GenericViewModel.cs
@@ -1,15 +1,13 @@
-﻿namespace neophyte.api.Models.View;
+﻿using Swashbuckle.AspNetCore.Annotations;
+
+namespace neophyte.api.Models.View;
 
 public class GenericViewModel
 {
+    /// <summary>
+    /// The error message
+    /// </summary>
+    /// <example>An error occurred</example>
+    [SwaggerSchema(Nullable = false)]
     public string Message { get; set; }
-
-    public GenericViewModel()
-    {
-    }
-
-    public GenericViewModel(string message)
-    {
-        Message = message;
-    }
 }
