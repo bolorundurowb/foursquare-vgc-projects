f9f894b7ebf363aec2b5be2f85a009262b1a3eba
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-02-04 10:44:33 +0100

    ft: add in the person model docs

--- api/neophyte-api/Models/View/BasePersonViewModel.cs
+++ api/neophyte-api/Models/View/BasePersonViewModel.cs
@@ -1,10 +1,25 @@
-﻿namespace neophyte.api.Models.View;
+﻿using Swashbuckle.AspNetCore.Annotations;
+
+namespace neophyte.api.Models.View;
 
 public class BasePersonViewModel
 {
+    /// <summary>
+    /// The church attendee internal identifier
+    /// </summary>
+    /// <example>123e4567-e89b-12d3-a456-426614174000</example>
+    [SwaggerSchema(Nullable = false)]
     public string Id { get; set; }
 
+    /// <summary>
+    /// The church attendee's first name
+    /// </summary>
+    /// <example>Jane</example>
     public string FirstName { get; set; }
 
+    /// <summary>
+    /// The church attendee's last name
+    /// </summary>
+    /// <example>Doe</example>
     public string LastName { get; set; }
 }
