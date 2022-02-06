e3bff9c39427d3599b945020bf9ce523865dc9ad
Oreofe <oreofe.olutola@gmail.com>
2022-02-06 19:56:32 +0100

    setup routing

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,48 +1,14 @@
 <template>
   <div class="page">
-    <div class="header">
-      <img src="./assets/logo.png"/>
-    </div>
-    <div class="container">
-      <router-view/>
-    </div>
+    <router-view/>
   </div>
 </template>
 
-<style scoped>
-.page {
-  padding: 2rem;
-}
-
-.page .header {
-  margin-top: 2rem;
-  margin-bottom: 3rem;
-  text-align: center;
-}
-
-.page .header img {
-  width: 15rem;
-}
-
-.page .container {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-}
-
-@media only screen and (max-device-width: 768px) {
-  .page {
-    padding: 1rem;
-  }
-
-  .page .header img {
-    width: 12rem;
-  }
-}
-</style>
-
 <style>
+.page {
+  min-height: 100vh;
+}
+
 .swal-button {
   padding: 0 3rem !important;
   border: 0.1rem solid #9b4dca !important;
