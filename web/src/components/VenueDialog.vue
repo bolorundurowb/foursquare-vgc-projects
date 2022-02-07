a53cfb33ef5fd76211665f6225f951646533669e
Oreofe <oreofe.olutola@gmail.com>
2022-02-07 19:58:54 +0100

    components for veue dialog and venue table

---
+++ web/src/components/VenueDialog.vue
@@ -0,0 +1,169 @@
+<template>
+  <el-dialog
+    title="Add new venue"
+    width="30%"
+    class="VenuesDialog"
+    :visible="showVenueForm"
+    :lock-scroll="false"
+    :center="true"
+    :destroy-on-close="true"
+    @close="handleClose"
+  >
+    <div class="VenuesDialog__content">
+      <el-form
+        :model="venueForm"
+        ref="venueForm"
+        label-position="top"
+      >
+        <el-form-item label="Name" prop="name" :rules="venueFormRules.name">
+          <el-input v-model="venueForm.name" />
+        </el-form-item>
+
+
+
+        <el-row
+          v-for="(range, index) in venueForm.seatRanges"
+          :key="index"
+          :gutter="20"
+          align="middle"
+          type="flex"
+        >
+          <el-col :span="12">
+            <el-form-item label="Seat/Seat Range" :rules="venueFormRules.numberRange" :prop="`numberRage-${index}`">
+              <el-input v-model="range.numberRange" placeholder="A12-A100/B12"/>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="Category" :rules="venueFormRules.category" :prop="`category-${index}`">
+              <el-select
+                v-model="range.category"
+                placeholder="Select a category"
+              >
+                <el-option
+                  v-for="item in categoryOptions"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="4">
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              circle
+              class="VenuesDialog__delete-button"
+              :disabled="index === 0"
+              @click="handleRemoveSeatCategory(index)"
+            />
+          </el-col>
+        </el-row>
+
+        <el-form-item>
+          <el-button size="small" plain type="primary" @click="handleAddSeatCategory">
+            Add Seat category
+          </el-button>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button
+            type="primary"
+            @click="handleAddVenue"
+            :disabled="isCreatingVenue"
+            v-loading="isCreatingVenue"
+          >
+            Add Venue
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name: 'VenueDialog',
+  props: {
+    showVenueForm: {
+      type: Boolean,
+      default: false
+    },
+    isCreatingVenue: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      venueForm: {
+        name: '',
+        seatRanges: [
+          {
+            category: '',
+            numberRange: ''
+          }
+        ]
+      },
+      venueFormRules: {
+        name: [
+          { required: true, message: 'Name is required' }
+        ],
+        numberRange: [
+          // { required: true, message: 'Number Range is required' }
+        ],
+        category: [
+          // { required: true, message: 'Category is required' }
+        ]
+      },
+      categoryOptions: [
+        {
+          value: 'Single',
+          label: 'Single'
+        },
+        {
+          value: 'Couples',
+          label: 'Couples'
+        }
+      ]
+    }
+  },
+  methods: {
+    handleRemoveSeatCategory(index) {
+      this.venueForm.seatRanges.splice(index, 1);
+    },
+    handleAddSeatCategory() {
+      this.venueForm.seatRanges.push({
+        category: '',
+        numberRange: ''
+      });
+    },
+    handleAddVenue() {
+      this.$refs.venueForm.validate((valid) => {
+        if (valid) {
+          this.$emit('create-venue', this.venueForm);
+        } else {
+          return false;
+        }
+      });
+    },
+    handleClose() {
+      this.$emit('close');
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .VenuesDialog {
+    &__content {
+      max-height: 500px;
+      overflow-y: auto;
+      overflow-x: hidden;
+    }
+
+    &__delete-button {
+      margin-top: 22px;
+    }
+  }
+</style>
--- web/src/views/Venues.vue
+++ web/src/views/Venues.vue
@@ -11,175 +11,48 @@
       </el-button>
     </el-empty>
 
-    <el-row class="Venues__header-row">
+    <el-row class="Venues__header-row" v-if="venues.length > 0">
       <el-button type="primary" @click="showVenueForm = true" size="small">
         <i class="el-icon-plus" />
         Add Venue
       </el-button>
     </el-row>
 
-    <el-table
-      stripe
-      style="width: 100%"
-      :data="venues"
-      v-loading="isLoadingVenues || isDeleteingVenue || isCreatingVenue"
+    <venue-table
+      :venues="venues"
+      :is-loading="isLoadingVenues || isDeleteingVenue || isCreatingVenue"
       v-if="venues.length > 0"
-    >
-      <el-table-column
-        prop="name"
-        label="Name" />
-      <el-table-column
-        prop="numOfSeats"
-        label="Number of seats" />
-      <el-table-column
-        label=""
-        width="120">
-        <template slot-scope="{ row }">
-          <el-button type="text" size="small">Details</el-button>
-          <el-popconfirm
-            title="Are you sure to delete this?"
-            class="Venues__table-delete-button"
-            icon-color="red"
-            confirm-button-text="Delete"
-            cancel-button-text="No"
-            @confirm="handleDeleteVenue(row)"
-          >
-            <el-button
-              slot="reference"
-              type="danger"
-              size="mini"
-              icon="el-icon-delete"
-              circle
-            />
-          </el-popconfirm>
-        </template>
-      </el-table-column>
-    </el-table>
+      @delete-venue="handleDeleteVenue"
+    />
 
-    <el-dialog
-      title="Add new venue"
-      width="30%"
-      class="VenuesDialog"
-      :visible.sync="showVenueForm"
-      :lock-scroll="false"
-      :center="true"
-      :destroy-on-close="true"
-    >
-      <div class="VenuesDialog__content">
-        <el-form
-          :model="venueForm"
-          ref="venueForm"
-          label-position="top"
-        >
-          <el-form-item label="Name" prop="name" :rules="venueFormRules.name">
-            <el-input v-model="venueForm.name" />
-          </el-form-item>
-
-
-
-          <el-row
-            v-for="(range, index) in venueForm.seatRanges"
-            :key="index"
-            :gutter="20"
-            align="middle"
-            type="flex"
-          >
-            <el-col :span="12">
-              <el-form-item label="Seat/Seat Range" :rules="venueFormRules.numberRange" :prop="`numberRage-${index}`">
-                <el-input v-model="range.numberRange" placeholder="A12-A100/B12"/>
-              </el-form-item>
-            </el-col>
-            <el-col :span="8">
-              <el-form-item label="Category" :rules="venueFormRules.category" :prop="`category-${index}`">
-                <el-select
-                  v-model="range.category"
-                  placeholder="Select a category"
-                >
-                  <el-option
-                    v-for="item in categoryOptions"
-                    :key="item.value"
-                    :label="item.label"
-                    :value="item.value">
-                  </el-option>
-                </el-select>
-              </el-form-item>
-            </el-col>
-            <el-col :span="4">
-              <el-button
-                type="danger"
-                icon="el-icon-delete"
-                circle
-                class="VenuesDialog__delete-button"
-                :disabled="index === 0"
-                @click="handleRemoveSeatCategory(index)"
-              />
-            </el-col>
-          </el-row>
-
-          <el-form-item>
-            <el-button size="small" plain type="primary" @click="handleAddSeatCategory">
-              Add Seat category
-            </el-button>
-          </el-form-item>
-
-          <el-form-item>
-            <el-button
-              type="primary"
-              @click="handleAddVenue"
-              :disabled="isCreatingVenue"
-              v-loading="isCreatingVenue"
-            >
-              Add Venue
-            </el-button>
-          </el-form-item>
-        </el-form>
-      </div>
-    </el-dialog>
+    <!-- dialog here -->
+    <venue-dialog
+      :show-venue-form="showVenueForm"
+      :is-creating-venue="isCreatingVenue"
+      @create-venue="handleAddVenue"
+      @close="showVenueForm = false"
+    />
   </div>
 </template>
 
 <script>
 import api from '@/utils/api';
+import VenueTable from '@/components/VenueTable.vue'
+import VenueDialog from '@/components/VenueDialog.vue'
 
 export default {
   name: 'Venues',
+  components: {
+    VenueTable,
+    VenueDialog
+  },
   data() {
     return {
       isLoadingVenues: false,
       isCreatingVenue: false,
       isDeleteingVenue: false,
       venues: [],
-      showVenueForm: false,
-      venueForm: {
-        name: '',
-        seatRanges: [
-          {
-            category: '',
-            numberRange: ''
-          }
-        ]
-      },
-      venueFormRules: {
-        name: [
-          { required: true, message: 'Name is required' }
-        ],
-        numberRange: [
-          // { required: true, message: 'Number Range is required' }
-        ],
-        category: [
-          // { required: true, message: 'Category is required' }
-        ]
-      },
-      categoryOptions: [
-        {
-          value: 'Single',
-          label: 'Single'
-        },
-        {
-          value: 'Couples',
-          label: 'Couples'
-        }
-      ]
+      showVenueForm: false
     };
   },
   methods: {
@@ -189,7 +62,6 @@ export default {
       try {
         const { data } = await api.get('/v1/venues');
 
-        console.log(data, 'data')
         this.venues = data;
       } catch (error) {
         const { data, status } = error.response;
@@ -231,24 +103,9 @@ export default {
         this.isDeleteingVenue = false;
       }
     },
-    handleAddSeatCategory() {
-      this.venueForm.seatRanges.push({
-        category: '',
-        numberRange: ''
-      });
-    },
-    handleRemoveSeatCategory(index) {
-      this.venueForm.seatRanges.splice(index, 1);
-    },
-    handleAddVenue() {
-      this.$refs.venueForm.validate((valid) => {
-        console.log(this.venueForm, 'venue form')
-        if (valid) {
-          this.createVenue(this.venueForm);
-        } else {
-          return false;
-        }
-      });
+
+    handleAddVenue(data) {
+      this.createVenue(data);
     },
     handleDeleteVenue({ id }) {
       this.deleteVenue(id);
@@ -264,21 +121,6 @@ export default {
 .Venues {
   &__header-row {
     margin-bottom: 20px;
-  }
-
-  &__table-delete-button {
-    margin-left: 7px;
-  }
-}
-.VenuesDialog {
-  &__content {
-    max-height: 500px;
-    overflow-y: auto;
-    overflow-x: hidden;
-  }
-
-  &__delete-button {
-    margin-top: 22px;
   }
 }
 </style>
