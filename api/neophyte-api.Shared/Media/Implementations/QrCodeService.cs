02bfbaaeca37db59c2b53115fdbbbde1b89a6af4
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2022-02-19 21:37:44 +0100

    ft: change variable names

--- api/neophyte-api.Shared/Media/Implementations/QrCodeService.cs
+++ api/neophyte-api.Shared/Media/Implementations/QrCodeService.cs
@@ -5,11 +5,11 @@ namespace neophyte.api.Shared.Media.Implementations;
 
 public static class QrCodeService
 {
-    private static readonly QRCodeGenerator _generator = new();
+    private static readonly QRCodeGenerator Generator = new();
 
     public static string GenerateQrCode(string payload)
     {
-        var data = _generator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+        var data = Generator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
         var qrCode = new PngByteQRCode(data);
         var binary = qrCode.GetGraphic(20);
         return Convert.ToBase64String(binary);
