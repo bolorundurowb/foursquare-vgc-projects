5128aae3acd2eb04d71ea0e3981cca8e5d397521
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2023-11-05 18:05:54 +0100

    ft: add attendance registry model

---
+++ api/neophyte-api.Data/Entities/AttendanceRegistry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using meerkat;
+using meerkat.Attributes;
+
+namespace neophyte.api.Data.Entities;
+
+[Collection(Name = "attendance-registry", TrackTimestamps = true)]
+public class AttendanceRegistry : Schema
+{
+    public DateOnly Date { get; private set; }
+
+    public List<Attendee> Attendees { get; private set; }
+
+    public AttendanceRegistry()
+    {
+        Date = DateOnly.FromDateTime(DateTime.UtcNow);
+        Attendees = new List<Attendee>();
+    }
+}
