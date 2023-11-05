5aee885d514f88ad0ee4f5709d63b365625aa306
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2023-11-05 18:07:45 +0100

    ft: remve the attendee csv mapper

--- api/neophyte-api.Data/Generators/CsvGenerator.cs
+++ api/neophyte-api.Data/Generators/CsvGenerator.cs
@@ -11,24 +11,6 @@ namespace neophyte.api.Data.Generators;
 
 public static class CsvGenerator
 {
-    public static async Task<byte[]> ForAttendees(List<Attendee> attendees)
-    {
-        // add in the serial numbers, since the csv mapper doesnt work
-        var attendanceSerialNo = 1;
-        attendees.ForEach(x =>
-        {
-            x.SerialNo = attendanceSerialNo;
-            attendanceSerialNo++;
-        });
-
-        await using var writer = new StringWriter();
-        await using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
-        csv.Context.RegisterClassMap<AttendeeCsvMapper>();
-        await csv.WriteRecordsAsync(attendees);
-        var csvString = writer.ToString();
-        return Encoding.UTF8.GetBytes(csvString);
-    }
-
     public static async Task<byte[]> ForNewcomers(List<Newcomer> newcomers)
     {
         // add in the serial numbers, since the csv mapper doesnt work
